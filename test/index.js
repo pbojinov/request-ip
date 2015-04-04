0338b2440d7c8e2bdaa61fb54bb448f8382609b7
Petar Bojinov <petarbojinov@gmail.com>
2015-04-03 19:22:25 -0700

    set up each test with its own server to make requests to to avoid race conditions and tests hanging

---
+++ test/index.js
@@ -0,0 +1,99 @@
+var http = require('http');
+var test = require('tape');
+var request = require('request');
+var requestIp = require('../index.js');
+
+// Setup local server for testing
+var serverInfo = {
+    host: '127.0.0.1'
+};
+
+function serverFactory() {
+    return http.createServer(function(req, res) {
+        // res.writeHead(200, {'Content-Type': 'text/plain'});
+        var ip = requestIp.getClientIp(req);
+        res.end(ip);
+    });
+}
+
+test('x-client-ip', function(t) {
+    t.plan(1);
+    var options = {
+        url: '',
+        headers: {
+            'x-client-ip': '59.195.114.48'
+        }
+    };
+    // create new server for each test so we can easily close it after the test is done
+    // prevents tests from hanging and competing against closing a global server
+    var server = new serverFactory();
+    // node listens on a random port when using 0
+    // http://stackoverflow.com/questions/9901043/how-does-node-js-choose-random-ports
+    server.listen(0, serverInfo.host);
+    server.on('listening', function() {
+        // we can't make the request URL until we get the port number from the new server
+        options.url = 'http://' + serverInfo.host + ':' + server.address().port;;
+        request(options, callback);
+    });
+
+    function callback(error, response, body) {
+        if (!error && response.statusCode === 200) {
+            // make sure response ip is the same as the one we passed in
+            t.equal(options.headers['x-client-ip'], body);
+            server.close();
+        }
+    }
+});
+
+test('x-forwarded-for', function(t) {
+    t.plan(1);
+    var options = {
+        url: '',
+        headers: {
+            'x-forwarded-for': '129.78.138.66, 129.78.64.103, 129.78.64.105'
+        }
+    };
+    // create new server for each test so we can easily close it after the test is done
+    // prevents tests from hanging and competing against closing a global server
+    var server = new serverFactory();
+    server.listen(0, serverInfo.host);
+    server.on('listening', function() {
+        options.url = 'http://' + serverInfo.host + ':' + server.address().port;;
+        request(options, callback);
+    });
+
+    function callback(error, response, body) {
+        if (!error && response.statusCode === 200) {
+            // make sure response ip is the same as the one we passed in
+            t.equal(options.headers['x-forwarded-for'], body);
+            server.close();
+        }
+    }
+});
+
+test('x-real-ip', function(t) {
+    t.plan(1);
+    var options = {
+        url: '',
+        headers: {
+            'x-real-ip': '129.78.138.66'
+        }
+    };
+    // create new server for each test so we can easily close it after the test is done
+    // prevents tests from hanging and competing against closing a global server
+    var server = new serverFactory();
+    server.listen(0, serverInfo.host);
+    server.on('listening', function() {
+        options.url = 'http://' + serverInfo.host + ':' + server.address().port;;
+        request(options, callback);
+    });
+
+    function callback(error, response, body) {
+        if (!error && response.statusCode === 200) {
+            // make sure response ip is the same as the one we passed in
+            t.equal(options.headers['x-real-ip'], body);
+            server.close();
+        }
+    }
+});
+
